f58b028bfa5f4e066111f9426b0e5afeafa50522
Lucas Petit <lucas.noirotorion@gmail.com>
2026-06-03 18:35:54 +0200

    refactor(types): enhance type definitions and documentation for clarity

--- src_cpp/common/types.hpp
+++ src_cpp/common/types.hpp
@@ -1,14 +1,41 @@
-// #include "common/types.hpp"
-
+/**
+ * @file types.hpp
+ * @brief Définitions des types primitifs.
+ * Usage: #include "common/types.hpp"
+ */
 #pragma once
 
 #include <cstdint>
 
-// marqueurs temporels
-using TickCount = uint8_t;
+/**
+ * @brief Identifiant discret d'un pas de temps.
+ * Les instances ne dépassent pas 2 ticks.
+ */
 using Tick = uint8_t;
 
-// marqueurs noeuds
+/**
+ * @brief L'unité du temps.
+ */
+using TickCount = uint8_t;
 
-using NodeCount = uint16_t;
+/**
+ * @brief Identifiant unique d'un nœud.
+ * Les instances ne dépassent pas 400 noeuds.
+ */
 using NodeId = uint16_t;
+
+/**
+ * @brief Représente le nombre total de nœuds présents dans le graphe.
+ */
+using NodeCount = uint16_t;
+
+/**
+ * @brief Identifiant unique d'un arc.
+ * Les instances ne dépassent pas 2 000 arcs.
+ */
+using EdgeId = uint16_t;
+
+/**
+ * @brief Représente le nombre total d'arcs présents dans le graphe.
+ */
+using EdgeCount = uint16_t;
